0063919735bf7b7c3bbd0bc4ea1545fc1988b7b7
zhangjianfei-2007 <zhangjianfei@msn.com>
2022-07-12 14:17:41 +0800

    build: refine build to use unified env envirable

--- build_linux.sh
+++ build_linux.sh
@@ -1,12 +1,11 @@
 
-## HOME DIR is the directory of the whol project, it is hardcoded in paddle-lite cmakefile.txt
-## please change the home dir also in cmakefile.txt same with this home dir
-## TOOL_CHAIN is the location toolchain is existed, for vs680, it use gcc-arm-linux-gnueabihf
+## HOME DIR is the directory of the project, the default is the upperlevel of current paddle-lite
+## change the home dir to other directory if you want other locatio to save download tim-vx code.
+## never set home_dir to / for permission issue
+## TOOL_CHAIN is the location of toolchain, for vs680 it use gcc-arm-linux-gnueabihf
 
-export HOME_DIR=~/project/vs680
-
+export HOME_DIR=`pwd`/..
 export TOOL_CHAIN=~/project/vs680/Synaptics_Git_Ext_Linux/syna-release/toolchain/aarch32/gcc-arm-linux-gnueabi-8.3
-
 export PATH=$PATH:${TOOL_CHAIN}/bin
 export ARCH=arm32
 export CROSS_COMPILE=arm-linux-gnueabihf-
